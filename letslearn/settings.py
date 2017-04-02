9bc58e34c0da5280b34be08e9d13f5759700e719
Paul K. George <paulkottalil20@gmail.com>
2017-04-03 00:04:36 +0530

    created basic model

--- letslearn/settings.py
+++ letslearn/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles
-    'classroom',
-    ]
+    'django.contrib.staticfiles',
+    'classroom'
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
